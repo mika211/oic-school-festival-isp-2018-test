238214c7a342268320cbc98ef148441d0c7c0c10
kaneko <mika021195@gmail.com>
2018-06-05 08:56:56 +0900

    test remove JpaRepository

--- src/main/java/schoolFestival/domain/repository/SalesRepository.java
+++ src/main/java/schoolFestival/domain/repository/SalesRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import schoolFestival.domain.model.Sales;
 
-public interface SalesRepository extends JpaRepository<Sales, Integer> {
+public interface SalesRepository /*extends JpaRepository<Sales, Integer>*/ {
 
 }
--- src/main/java/schoolFestival/domain/repository/StudentRepository.java
+++ src/main/java/schoolFestival/domain/repository/StudentRepository.java
@@ -1,9 +1,15 @@
 package schoolFestival.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import schoolFestival.domain.model.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Integer> {
+public interface StudentRepository /*extends JpaRepository<Student, Integer>*/ {
+	
+	@Query(value="select id, name from t_student")
+	List<Student> findAll();
 
 }
