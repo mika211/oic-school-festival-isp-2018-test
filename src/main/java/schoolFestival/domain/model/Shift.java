2d9a7d720b3875444b4c57a18f38cffa75667975
kaneko <mika021195@gmail.com>
2018-06-05 11:43:50 +0900

    test fixed print shiftList

--- src/main/java/schoolFestival/domain/model/Shift.java
+++ src/main/java/schoolFestival/domain/model/Shift.java
@@ -7,11 +7,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import lombok.Data;
 import lombok.ToString;
@@ -32,7 +36,9 @@ public class Shift implements Serializable {
 	private Integer id;
 	
 	/** 学生 */
+	@Fetch(FetchMode.JOIN)
 	@ManyToOne
+	@JoinColumn(name="student_id")
 	private Student student;
 
 	/** 開始日時 */
