d3b8d88a77ca13876b56f328a26c89235ecee784
kaneko <mika021195@gmail.com>
2018-06-05 12:51:38 +0900

    fixed @DataJpaTest

--- src/test/java/schoolFestival/domain/repository/ShiftRepositoryTest.java
+++ src/test/java/schoolFestival/domain/repository/ShiftRepositoryTest.java
@@ -3,9 +3,12 @@ package schoolFestival.domain.repository;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 public class ShiftRepositoryTest {
 	
