143e23d3b820ede4f15873fab66f92c12d9af785
kaneko <mika021195@gmail.com>
2018-06-06 09:28:06 +0900

    remove import DataJpaTest

--- src/main/java/schoolFestival/domain/service/TestService.java
+++ src/main/java/schoolFestival/domain/service/TestService.java
@@ -3,7 +3,6 @@ package schoolFestival.domain.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.stereotype.Service;
 
 import schoolFestival.domain.model.Shift;
