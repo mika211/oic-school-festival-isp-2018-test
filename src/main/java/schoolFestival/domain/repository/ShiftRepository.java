838e4d33cae0036ec74458be02a2e1ce0863dade
kaneko <mika021195@gmail.com>
2018-06-06 10:10:21 +0900

    test join fetch

--- src/main/java/schoolFestival/domain/repository/ShiftRepository.java
+++ src/main/java/schoolFestival/domain/repository/ShiftRepository.java
@@ -1,9 +1,16 @@
 package schoolFestival.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import schoolFestival.domain.model.Shift;
 
 public interface ShiftRepository extends JpaRepository<Shift, Integer> {
 
+	@Override
+	@Query(value="SELECT * FROM t_shift sh JOIN FETCH sh.student_id")
+	List<Shift> findAll();
+	
 }
