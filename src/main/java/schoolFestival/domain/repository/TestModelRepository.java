bcac5671324b9a71db38f91ae44774012339abe7
kaneko <mika021195@gmail.com>
2018-06-07 18:09:11 +0900

    fixed TestModelRepository

--- src/main/java/schoolFestival/domain/repository/TestModelRepository.java
+++ src/main/java/schoolFestival/domain/repository/TestModelRepository.java
@@ -11,7 +11,7 @@ import schoolFestival.domain.model.TestModel;
 
 public interface TestModelRepository extends JpaRepository<TestModel, Integer> {
 
-	@Query("select new TestModel(date_trunc('hour', w.datetime), SUM(w.amount)) from (select s(s.id, s.datetime, s.discount, (sum(p.price * sd.qty) - s.discount)) from Sales s join SalesDetail sd on s = sd.sales join ProductPrice p on sd.productPrice = p group by s.id, s.datetime, s.discount) as w group by 1 order by 1")
+	@Query("select new TestModel(date_trunc('hour', w.datetime), sum(w.amount)) from (select (s.id, s.datetime, s.discount, (sum(p.price * sd.qty) - s.discount)) from schoolFestival.domain.model.Sales s join schoolFestival.domain.model.SalesDetail sd on s = sd.sales join schoolFestival.domain.model.ProductPrice p on sd.productPrice = p group by s.id, s.datetime, s.discount) as w group by 1 order by 1")
 	List<TestModel> findAll();
 	
 }
